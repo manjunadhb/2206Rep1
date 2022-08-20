ca4120c4dfcbebdfc3a4527c761bc9038cff65fc
sathwic <sathwimc97@gmail.com>
2022-08-20 21:44:53 +0530

    cleared unwanted data

--- pulse-app/src/App.js
+++ pulse-app/src/App.js
@@ -1,67 +1,7 @@
- 
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Manjunadh</h1>
-
-      {/* Students of 2206 Batch. */}
-      <h1>Shaikh Waqar</h1>
-      <h1>shankar santosh gorle</h1>
-
-      {/* Venkatesh Info */}
-      <h1> Venkatesh </h1>
-      <ol>
-        <li>Full Name - V.Venkateswarlu</li>
-        <li>DOB - 06/09/1999</li>
-        <li>Qualification - Degree</li>
-        <li>Dream - A Well settled Life</li>
-        <li>About Me - Introvert,creative learning and enthusiastic person</li>
-        <li>Habits - Road Trips,Eating and Learning new things</li>
-      </ol>
-      {/*-------------------------------*/}
-      <h1> Shaikh Waqar </h1>
-
-      {/* SATHWIC RAJ */}
-      <h1> Sathwic </h1>
-
-{/*shreehari Information */}
-      <h1>shreehari</h1>
-      <ol>
-        <li>Full Name - Shreehari</li>
-        <li>Native Place - Karnataka</li>
-        <li>Qualification - BE(Mechanical)</li>
-      </ol>
-      {/*----------------------*/}
-      
-     <h1> Vinay kumar </h1>
-=======
-      <ul>
-        <li>Date of Birth : 04/12/1997.</li>
-        <li>Qualification : B.Com(Computer Applications).</li>
-        <li>
-          About Myself : Curious, Determined, Open-minded and an Existential
-          Nihilist. Always ready for adventures.
-        </li>
-      </ul>
-      {/*-------------------------------*/}
-
-      <h1>shreehari</h1>
-      <h1> Vinay kumar </h1>
-
-      <h1>G Priyanka</h1>
-      <h1>karthik</h1>
-      <h1>Masarath Anjum</h1>
-      <h1>Sandeep</h1>
-      <h1>Prashanth</h1>
-      <h1>Vikram</h1>
-      <h1>Heading-MyName</h1>
-      <h1>Hitesh Kumar</h1>
-      <h1>Masrad</h1>
-     
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
