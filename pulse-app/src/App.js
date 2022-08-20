d92079e2f3852dba751d0e1ddeeed57f2550b4a6
sathwic <sathwimc97@gmail.com>
2022-08-20 16:45:40 +0530

    added heading(comment)

--- pulse-app/src/App.js
+++ pulse-app/src/App.js
@@ -6,20 +6,17 @@ function App() {
     <div className="App">
       <h1>Manjunadh</h1>
 
+      {/* Students of 2206 Batch. */}
       <h1>Shaikh Waqar</h1>
-      <h2>Masarath Anjum</h2>
       <h1>shankar santosh gorle</h1>
       <h1> Venkatesh </h1>
       <h1> Shaikh Waqar </h1>
       <h1> Sathwic </h1>
-
       <h1>shreehari</h1>
-     <h1> Vinay kumar </h1>
+      <h1> Vinay kumar </h1>
       <h1>G Priyanka</h1>
       <h1>karthik</h1>
       <h1>Masarath Anjum</h1>
-
-
     </div>
   );
 }
