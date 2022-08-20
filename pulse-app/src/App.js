365a03462b09b7b31aea05c647b0b3bcebc76fa1
sathwic <sathwimc97@gmail.com>
2022-08-20 15:57:31 +0530

    added my name

--- pulse-app/src/App.js
+++ pulse-app/src/App.js
@@ -4,12 +4,10 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Manjunadh</h1>
-
-      <h1>Venkatesh</h1>
-
-      <h1>Shaikh Waqar</h1>
-
+      <h1> Manjunadh </h1>
+      <h1> Venkatesh </h1>
+      <h1> Shaikh Waqar </h1>
+      <h1> Sathwic </h1>
     </div>
   );
 }
