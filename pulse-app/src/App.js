587a92288d253105756c522cae4920f7fead7d62
shreehari <shreeharit1@gmail.com>
2022-08-20 20:32:29 +0530

    adding information about me

--- pulse-app/src/App.js
+++ pulse-app/src/App.js
@@ -9,8 +9,15 @@ function App() {
       <h1> Venkatesh </h1>
       <h1> Shaikh Waqar </h1>
       <h1> Sathwic </h1>
-
+{/*shreehari information */}
       <h1>shreehari</h1>
+      <ol>
+        <li>Full Name - Shreehari</li>
+        <li>Native Place - Karnataka</li>
+        <li>Qualification - BE(Mechanical)</li>
+      </ol>
+      {/*----------------------*/}
+      
      <h1> Vinay kumar </h1>
       <h1>G Priyanka</h1>
       <h1>karthik</h1>
